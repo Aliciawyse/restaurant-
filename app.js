d541846ceeb9e23498f22d909137f2f003adcdce
Barrett <Aliciawyse@users.noreply.github.com>
2017-11-02 20:18:52 -0400

    successfully connecting to local server

--- app.js
+++ app.js
@@ -14,3 +14,52 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// populate tables
+var tables = [
+    {
+        routeName: "table1",
+        name: "Guest One",
+        id: 01
+    },
+    {
+        routeName: "table2",
+        name: "Guest Two",
+        id: 02
+    },
+    {
+        routeName: "table3",
+        name: "Guest Three",
+        id: 03
+    },
+    {
+        routeName: "table4",
+        name: "Guest Four",
+        id: 04
+    },
+    {
+        routeName: "table5",
+        name: "Guest Five",
+        id: 05
+    }
+];
+
+//routes
+
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,"index.html"));
+});
+
+
+app.get("/table", function(req,res){
+    res.sendFile(path.join(__dirname,"table.html"));
+});
+
+
+app.get("/reservation", function(req,res){
+    res.sendFile(path.join(__dirname,"reservation.html"));
+});
+
+//listen
+app.listen(PORT, function () {
+    console.log("Listening on port " + PORT);
+});
